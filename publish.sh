d5637bbba1bf7b7a7543ef850a1a45f0091052c5
陈逢隽 <raychenfj@gmail.com>
2018-12-07 09:27:47 +0800

    ci: mount tasks.json

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@ git pull origin master && \
 docker rm -f nomisma || true && \
 docker rmi nomisma || true && \
 docker build -t nomisma . && \
-docker run -d --name nomisma  -e NODE_ENV=production -v $HOME/logs/nomisma:/app/logs -p 3007:3000 nomisma"
+docker run -d --name nomisma  -e NODE_ENV=production -v $HOME/logs/nomisma:/app/logs -v $HOME/tasks.json:/app/schedule/tasks.json -p 3007:3000 nomisma"
